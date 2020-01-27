0873c6bdc3efb8b24a04a60b312403e5e7eaba3f
breizh5729 <vincn.ledan@gmail.com>
2020-01-27 20:57:52 +0100

    final update

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_khtaz3NYj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'qlv307t3f6esipuqmshgsd4n5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_FWpXcjB9F', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2c17omuoq6mb3qd7dgl441eluu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2g
     },
     api: {
-        invokeUrl: 'https://if9gx8ayv6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://l4ijsho1f1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'vincent.ledan@continuous.team'
