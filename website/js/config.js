e6d13b4e22208df57feced7a7e5858a4a2ae7008
breizh5729 <vincn.ledan@gmail.com>
2020-01-27 23:14:50 +0100

    dd

--- website/js/config.js
+++ website/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2g
     },
     api: {
-        invokeUrl: 'https://l4ijsho1f1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://lc5f5ir6d6.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'vincent.ledan@continuous.team'
